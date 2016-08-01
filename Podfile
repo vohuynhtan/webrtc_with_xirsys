ed8df80ab58436f0f217626cd9f8271c4fb93d79
tanvohuynh <tan.vo@elinext.com>
2016-08-01 23:05:56 +0700

    First Project

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WebRTC-Xirsys' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WebRTC-Xirsys
+  pod 'Alamofire', '~> 3.4'
+  pod 'libjingle_peerconnection'
+  
+end
---
+++ WebRTC-Xirsys/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebRTC-Xirsys
+//
+//  Created by Tan Vo on 8/1/16.
+//  Copyright © 2016 Tan Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
